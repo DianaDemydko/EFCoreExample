24256c212635fa145ddbb9a78891afb18257beed
Patcharin Prakrajang <patcharin.prakrajang@nexsource.net>
2021-03-23 13:21:25 +0200

    first commit

---
+++ DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("BookS");
+        }
+    }
+}
